90ea9091ffd0cb03ab55a0b8de95a25e46cca2ac
Mile <milevuletic@national.shitposting.agency>
2020-07-03 01:26:06 +0200

    Added limited markdown unconverter

--- lib.js
+++ lib.js
@@ -41,16 +41,43 @@ const handleMessages = ( self, ms ) => {
 
 const reduceHtml = m => {
     const mp = m;
-    // <p>
-    //mp.message = m.message.substring( 3, m.message.length-5 );
 
+    const unescapeHtml = unsafe => {
+        return unsafe
+            .replace( /&amp;/g,  `&` )
+            .replace( /&lt;/g,   `<` )
+            .replace( /&gt;/g,   `>` )
+            .replace( /&quot;/g, `"` )
+            .replace( /&#39;/g,  `'` );
+    };
+
+    // <blockquote> -> green text
+    mp.message = mp.message.replace(  /<\/?blockquote>/g, "" );
+    mp.message = unescapeHtml( mp.message );
+
+    // <span> -> 8chan green text
+    mp.message = mp.message.replace(  /<\/?span[\w =#"':\/\\.\-?]*>/gi, "" );
+
+    // <p></p>
+    mp.message = mp.message.replace( /<\/?p[\w =#"':\/\\.\-?]*>/gi, "" );
+
+    // TODO: investigate if custom links can appear
     // <a>gets left</a>
-    mp.message = mp.message.replace( /<\/?a[\w ="':\/\\.]*>/gi, "" );
+    mp.message = mp.message.replace( /<\/?a[\w =#"':\/\\.]*>/gi, "" );
+
+    mp.message = mp.message.replace( "<h1>", "# " );
+    mp.message = mp.message.replace( "<h2>", "## " );
+    mp.message = mp.message.replace( "<h3>", "### " );
+    mp.message = mp.message.replace( "<h4>", "#### " );
+    mp.message = mp.message.replace( "<h5>", "##### " );
+
+    mp.message = mp.message.replace( /<\/h[1-5]/, "" );
+
 
     // <img> to :emotes:
-    //mp.message = mp.message.replace( /<img[\w ="':\/\\.\-?]*>/gi, m => {
-    //    return m.search( /https:\/\/[\/\w\.\-?]*/ );
-    //});
+    mp.message = mp.message.replace( /<img[\w =#"':\/\\.\-?]*>/gi, m => {
+        return m.match( /:\w+:/ )[0];
+    });
     return mp;
 };
 
